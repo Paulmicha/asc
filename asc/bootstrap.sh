6784fe8d53a47ff0e61077a8561587ec0fecdccc
Paulmicha <paul.michalet@gmail.com>
2018-01-17 21:59:01 -0200

    Optimize asc/bootstrap.sh.

--- asc/bootstrap.sh
+++ asc/bootstrap.sh
@@ -3,10 +3,9 @@
 ##
 # Bootstraps ASC.
 #
-# Loads includes containing bash functions + call optional bootstrap hooks.
-#
-# TODO [wip] evaluating not resetting these globals on every call to bootstrap.
-# This is to allow presets to be organized like the asc folder (extensibility).
+# Loads includes containing bash functions along with readonly global vars if
+# available, initializes "primitives" for hooks and lookups (ASC extension
+# mecanisms), and call 'bootstrap' hook used to load bash aliases.
 #
 # @example
 #   . asc/bootstrap.sh
@@ -17,39 +16,42 @@ if [[ -z "$asc_bs_flag" ]]; then
   asc_bs_flag=1
 
   # Include "core" utilities.
-  for file in $(find asc/utilities -maxdepth 1 -type f -print0 | xargs -0); do
-    . "$file"
-  done
+  . "asc/utilities/array.sh"
+  . "asc/utilities/autoload.sh"
+  . "asc/utilities/asc.sh"
+  . "asc/utilities/fs.sh"
+  . "asc/utilities/global.sh"
+  . "asc/utilities/hook.sh"
+  . "asc/utilities/host.sh"
+  . "asc/utilities/instance.sh"
+  . "asc/utilities/once.sh" # TODO evaluate removal / make opt-in.
+  . "asc/utilities/registry.sh" # TODO evaluate removal / make opt-in.
+  . "asc/utilities/string.sh"
 
   # If stack init was run at least once, automatically load global env vars.
+  # NB : this must happen before u_asc_extend() gets called because it uses the
+  # customizable global var ASC_CUSTOM_DIR to populate primitive values.
   if [[ -f "asc/env/current/global.vars.sh" ]]; then
     . asc/env/load.sh
   fi
 
-  # TODO [wip] workaround instance state limitations (e.g. unhandled shutdown).
-  u_instance_get_state
-
   # Initializes "primitives" for hooks and lookups (ASC extension mecanisms).
-  # These are : subjects, actions, prefixes, variants and presets.
+  # These are : subjects, actions, prefixes, variants and extensions.
   u_asc_extend
 
-  # Load optional additional includes.
+  # Load additional includes (including extensions').
   if [[ -n "$ASC_INC" ]]; then
     for file in $ASC_INC; do
       . "$file"
+      # Any additional include may be altered using the 'complement' pattern.
       u_autoload_get_complement "$file"
     done
   fi
 
-  # Load bash aliases.
+  # Bash aliases should be loaded by implementing the 'bootstrap' action hook.
   # NB: aliases are not expanded when the shell is not interactive, unless the
   # expand_aliases shell option is set using shopt.
   # See https://unix.stackexchange.com/a/1498
   shopt -s expand_aliases
-  # TODO [wip] Refacto hooks to follow u_asc_extend().
-  u_hook_app 'bash' 'alias'
-
-  # Call any 'bootstrap' hooks.
-  # TODO [wip] Refacto hooks to follow u_asc_extend().
-  u_hook 'asc' 'bootstrap'
+  hook -a 'bootstrap'
 fi
