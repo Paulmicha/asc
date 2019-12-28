d27acfa309f388a353a508a847fb279864d56ddf
Paulmicha <paul.michalet@gmail.com>
2019-12-28 10:59:48 -0300

    Implement u_mysql_extract_from_dump().

--- asc/instance/instance.inc.sh
+++ asc/instance/instance.inc.sh
@@ -175,7 +175,7 @@ u_instance_init() {
 # @see u_instance_yaml_config_parse()
 #
 # To verify which files can be used (besides ".asc.yml" in this project
-# instance's docroot folder $PROJECT_DOCROOT), run :
+# instance's docroot folder $PROJECT_DOCROOT + its variants), run :
 # $ make hook-debug s:instance a:.asc c:yml v:HOST_TYPE INSTANCE_TYPE
 #
 # @example
