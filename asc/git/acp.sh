0acbb0d5135e0e04df6ca1c81f0aefedb22175e8
Paul <paul.michalet@gmail.com>
2026-09-12 12:47:59 -0300

    Add git acp convenience shortcut entry point.

--- asc/git/acp.sh
+++ asc/git/acp.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/env bash
+
+##
+# Convenience git shortcut : stage all, commit with given message, and push.
+#
+# @example
+#   make git-acp "'Foobar commit message.'"
+#   # Or :
+#   asc/git/acp.sh "Foobar commit message."
+#
+
+. asc/bootstrap.sh
+
 git add .
 git commit -m "$@"
 git push
