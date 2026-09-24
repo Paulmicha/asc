70cfcaf83552333d080ad556e5a561ec0b51e51f
Paul <paul.michalet@gmail.com>
2026-09-24 13:06:04 -0300

    wip: backup progress

--- asc/instance/ds.sh
+++ asc/instance/ds.sh
@@ -3,6 +3,12 @@
 ##
 # TODO [wip] entry point stub for inline DSL interpreter.
 #
+# TODO Duplicates the following entry point -> only keep 1 :
+# @see asc/instance/replay.sh
+#
+# TODO but keep the possibility of filepath arg support,
+# and/or hook_ms() that could match implementations like 'path/to/foobar.dsl.txt'
+#
 # @example
 #   make ds '[echo(v-baz)]---v-output_file_path'
 #
---
+++ asc/instance/replay.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##
+# Replays any DSL record.
+#
+# TODO Duplicates the following entry point -> only keep 1 :
+# @see asc/instance/ds.sh
+#
+# @example
+#   make replay path/to/foobar.dsl.txt
+#   # Or :
+#   asc/instance/replay.sh path/to/foobar.dsl.txt
+#
+
+. asc/bootstrap.sh
+
+# TODO
