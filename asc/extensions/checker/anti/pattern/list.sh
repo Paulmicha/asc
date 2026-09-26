8bf4a83909376473aabd3b7c54c88346a6a048cf
Paul <paul.michalet@gmail.com>
2026-09-26 11:10:11 -0300

    wip: backup progress

---
+++ asc/extensions/checker/anti/pattern/list.sh
@@ -0,0 +1,2 @@
+# TODO this entry point fetches a list of anti-patterns that are susceptible to apply to any given "git diff" context.
+# If the git diff is ran on a "dev stack" repo work tree (= an ASC project instance repo), then the anti-patterns list to load would be "asc core" anti-patterns list, in `data/entities/anti-pattern/asc`.
---
+++ asc/extensions/checker/diff/inspect.sh
@@ -0,0 +1,2 @@
+# TODO this entry point runs over a git diff in order to spot potential anti-patterns.
+# It reads the registry of stored anti-patterns.
